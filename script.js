66f32e08534ba488fa89c1a9e19084070c148369
drestew <drestew48@gmail.com>
2022-04-27 18:37:56 -0400

    Add color change on hover (hard-coded)

--- script.js
+++ script.js
@@ -2,11 +2,18 @@
 const container = document.querySelector('.container')
 
 let div
-const num = 64
+let heightWidth = 16
+const num = heightWidth * heightWidth
 for (i = 0; i < num; i++) {
   div = document.createElement('div')
   container.appendChild(div)
   div.classList.add('grid-item')
 }
 
-// div.forEach(el => el.classList.add('.grd'))
+const changeColor = function () {
+  // this.classList.add('hovered')
+  this.style.background = "green"
+}
+
+const gridItems = document.querySelectorAll('.grid-item')
+gridItems.forEach(item => item.addEventListener('mouseover', changeColor))
